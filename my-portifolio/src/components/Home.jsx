e2fd9a8cd78d2e88f44f7022f127c8d0d42221fd
william <williamigorsilva@gmail.com>
2020-06-19 11:52:40 -0300

    Start Portifolio

---
+++ my-portifolio/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../layout/Home.css';
+
+const Home = () => {
+    return (
+        <div>
+            <span className="start">William Igor</span>
+        </div>
+    );
+}
+
+export default Home
