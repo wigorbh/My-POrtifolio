59cc9230c26e5dbed6a8bde4dfdf28d61fecc406
william <williamigorsilva@gmail.com>
2020-07-18 15:37:01 -0300

    Updates

---
+++ precache-manifest.65c84baab22cca85e5ad5fccd0e8f33c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d93685c0fcbb619ae08488fa9fb5497c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0677cbf4c9a04ce5d58",
+    "url": "/static/css/main.82f000ca.chunk.css"
+  },
+  {
+    "revision": "5b5c8c44356aca007166",
+    "url": "/static/js/2.9abdce59.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9abdce59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0677cbf4c9a04ce5d58",
+    "url": "/static/js/main.983ee422.chunk.js"
+  },
+  {
+    "revision": "f1e47aea1443d2634bf8",
+    "url": "/static/js/runtime-main.08a51ad9.js"
+  }
+]);
